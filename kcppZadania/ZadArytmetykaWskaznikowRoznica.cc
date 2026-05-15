66b98a8d19b1e5eda1749e7fe24f83123b668e18
AbbathDeath <death.abbath@yandex.ru>
2026-05-15 18:10:27 +0300

    dodalem zadanie 3.5

---
+++ kcppZadania/ZadArytmetykaWskaznikowRoznica.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+*Napisz program: Różnica między dwoma wskaźnikami
+Stwórz program, który definiuje tablicę 10 liczb całkowitych, a następnie oblicza różnicę między dwoma wskaźnikami wskazującymi na różne elementy tej tablicy.
+Nazwa: ZadArytmetykaWskaznikowRoznica.cc
+
+ */
+
+int main() {
+
+ int tablica [] = {1,2,3,4,5,6,7,8,9,10};
+ int* ptr = &tablica[4];
+ int* ptr2 = &tablica[9];
+
+ std::cout << "Roznica miedzy dwoma wskaznikami: "<< ptr2 - ptr << std::endl;
+}
