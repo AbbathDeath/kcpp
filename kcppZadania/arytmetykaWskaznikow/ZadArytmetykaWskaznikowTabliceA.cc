c84de90a5fdae79efe68a598b539a5384d1956f2
AbbathDeath <death.abbath@yandex.ru>
2026-05-31 13:46:58 +0300

    Dokonczyłem zadanie 3.8

--- kcppZadania/arytmetykaWskaznikow/ZadArytmetykaWskaznikowTabliceA.cc
+++ kcppZadania/arytmetykaWskaznikow/ZadArytmetykaWskaznikowTabliceA.cc
@@ -14,43 +14,64 @@ Nazwać program: ZadArytmetykaWskaznikowTabliceA.cc
  */
 
 int main() {
-  int tablica [10];
+    int tablica[10];
 
-  for (int i = 0; i < 10; i++) {
-    *(tablica + i) = i + 1 ;
-  }
-
-  std::cout << "Tablica: ";
-  for (int i = 0; i < 10; i++) {
-    std::cout << tablica[i] << " ";
-  }
-  std::cout << std::endl;
-
-  int *ptr = tablica;
-  int rozmiar = sizeof(tablica) / sizeof(tablica[0]);
-
-  int suma = 0;
-
-  while (ptr != tablica + rozmiar) {
-    suma += *ptr;
-    ptr++;
-  }
-
-  std::cout << "Suma elementow: "<< suma << std::endl;
-
-  int max = *tablica;
-
-  int *ptr1 = tablica + 1;
-
-  while (ptr1 != tablica + rozmiar) {
-    if (*ptr1 > max) {
-      max = *ptr1;
+    for (int i = 0; i < 10; i++) {
+        *(tablica + i) = i + 1;
     }
-    ptr1++;
-  }
 
-  std::cout << "Najwiekszy element: "<< max << std::endl;
+    std::cout << "Tablica: ";
+    for (int i = 0; i < 10; i++) {
+        std::cout << tablica[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int* ptr = tablica;
+    int rozmiar = sizeof(tablica) / sizeof(tablica[0]);
+
+    int suma = 0;
+
+    while (ptr != tablica + rozmiar) {
+        suma += *ptr;
+        ptr++;
+    }
+
+    std::cout << "Suma elementow: " << suma << std::endl;
+
+    int max = *tablica;
+
+    int* ptr1 = tablica + 1;
+
+    while (ptr1 != tablica + rozmiar) {
+        if (*ptr1 > max) {
+            max = *ptr1;
+        }
+        ptr1++;
+    }
+
+    std::cout << "Najwiekszy element: " << max << std::endl;
+
+    int* lewo = tablica;
+    int* prawo = tablica + rozmiar - 1;
+
+    while (lewo < prawo)
+    {
+        int zamiana = *lewo;
+        *lewo = *prawo;
+        *prawo = zamiana;
+        lewo++;
+        prawo--;
+    }
+
+    std::cout << "Odwrocona tablica: ";
+    for (int i = 0; i < rozmiar; i++) {
+        std::cout << tablica[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 
 }
+
 
 
