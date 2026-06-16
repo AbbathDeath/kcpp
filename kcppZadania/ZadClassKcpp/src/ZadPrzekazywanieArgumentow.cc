ab7f0767c32271a01a82f1d289f6a19149452b27
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 13:09:29 +0300

    przerobilem zadanie 2.1 do struktury klass

---
+++ kcppZadania/ZadClassKcpp/src/ZadPrzekazywanieArgumentow.cc
@@ -0,0 +1,43 @@
+#include "ZadPrzekazywanieArgumentow.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+void ZadPrzekazywanieArgumentow::Uruchom() {
+  int a = 2;
+  std:: cout << "Przez wartosc: " << zwracaniePrzezWartosc(a) << std:: endl;
+  std:: cout << "Prez referencje: " << zwracaniePrzezReferencje(a) << std::endl;
+  std:: cout << "Przez wskaznik: " << zwracaniePrzezWskaznik(&a) << std::endl;
+  std:: cout << "Tablica: " << std::endl;
+  std:: vector<int> liczby { 1, 2, 3, 4, 5 };
+  zwracanieTablicy(liczby);
+
+}
+
+std::string ZadPrzekazywanieArgumentow::Opis() {
+  return "Przekazywanie argumentow przez wartosc, referencje i wskaznik";
+}
+
+int ZadPrzekazywanieArgumentow::zwracaniePrzezWartosc(int x) {
+  int wynik =  x * x;
+  return wynik;
+}
+
+
+
+int ZadPrzekazywanieArgumentow::zwracaniePrzezReferencje(int &x) {
+  int wynik = x * x;
+  return wynik;
+}
+
+int ZadPrzekazywanieArgumentow::zwracaniePrzezWskaznik(int *x) {
+  int wynik = (*x) * (*x);
+  return wynik;
+}
+
+void ZadPrzekazywanieArgumentow::zwracanieTablicy(std:: vector<int> liczby) {
+  for (int i = 0; i < liczby.size(); i++) {
+    std:: cout << liczby[i];
+  }
+  std:: cout << std::endl;
+}
