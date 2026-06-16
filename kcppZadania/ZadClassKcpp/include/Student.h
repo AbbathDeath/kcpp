940a3d799f00be55e25893b52b6bdbbddd1ead05
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 13:07:07 +0300

    przerobilem zadanie 5.4 do struktury klass

--- kcppZadania/ZadClassKcpp/include/Student.h
+++ kcppZadania/ZadClassKcpp/include/Student.h
@@ -1,8 +1,28 @@
-//
-// Created by death on 16.06.2026.
-//
 
 #ifndef ZADCLASSKCPP_STUDENT_H
 #define ZADCLASSKCPP_STUDENT_H
+#include <string>
 
+class Student {
+public:
+  std::string imie;
+  Student();
+  ~Student();
+
+  Student(std::string imie, std::string kierunek, int id);
+
+  Student(int id, std::string imie, std::string kierunek);
+
+  void wyswietlDaneStudenta();
+
+  void ustawKierunek(std::string kierunek);
+  void ustawId(int id);
+  void wypiszStudent();
+
+private:
+  int id;
+
+protected:
+  std::string kierunek;
+};
 #endif //ZADCLASSKCPP_STUDENT_H
