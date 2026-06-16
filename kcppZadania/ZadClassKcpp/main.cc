81012330a0c7a1f123b8d204a50dffc285b084fb
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 11:41:13 +0300

    dodalem proste menu do testów

--- kcppZadania/ZadClassKcpp/main.cc
+++ kcppZadania/ZadClassKcpp/main.cc
@@ -4,7 +4,30 @@
 
 
 int main() {
-  ZadMetodyAbstrakcyjne obj1;
-  std::cout << obj1.Opis() << std::endl;
-  obj1.Uruchom();
+  int wybor;
+
+
+  do {
+    std::cout << "Prosze wybrać zadanie z listy i podać numer zadania" << std::endl;
+    std::cout << "1. Metody abstrakcyjne" << std::endl;
+    std::cout << "0. Wyjście" << std::endl;
+    std::cin >> wybor;
+
+    switch (wybor) {
+      case 0:
+        break;
+
+      case 1: {
+        ZadMetodyAbstrakcyjne obj1;
+        std::cout << obj1.Opis() << std::endl;
+        obj1.Uruchom();
+        break;
+      }
+
+      default:
+        std::cout << "Niepoprawny numer zadania" << std::endl;
+        break;
+    }
+  }
+  while (wybor != 0);
 }
