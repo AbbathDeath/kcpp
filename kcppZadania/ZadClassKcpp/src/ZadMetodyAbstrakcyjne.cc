6865ac2e48c4fa67653232169167062cfc0d06ba
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 10:07:31 +0300

    stworzylem plik zrodlowy ZadMetodyAbstrakcyjne

---
+++ kcppZadania/ZadClassKcpp/src/ZadMetodyAbstrakcyjne.cc
@@ -0,0 +1,11 @@
+#include "../include/ZadMetodyAbstrakcyjne.h"
+#include <string>
+
+void ZadMetodyAbstrakcyjne::Uruchom() {
+
+}
+
+std::string ZadMetodyAbstrakcyjne::Opis() {
+  return "Metody abstrakcyjne - pole i obwod figur";
+}
+
