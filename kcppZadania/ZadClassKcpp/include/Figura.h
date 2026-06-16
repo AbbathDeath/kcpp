17d9a2572890bff4d22ce2e0bf5d434c3425c741
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 10:34:51 +0300

    dodalem definicje figur abstrakcyjnych

---
+++ kcppZadania/ZadClassKcpp/include/Figura.h
@@ -0,0 +1,59 @@
+
+#ifndef ZADCLASSKCPP_FIGURA_H
+#define ZADCLASSKCPP_FIGURA_H
+
+class Figura {
+public:
+  virtual void Narysuj() = 0;
+  virtual double Pole() = 0;
+  virtual double Obwod() = 0;
+  virtual ~Figura();
+};
+
+class Trojkat : public Figura {
+private:
+  double a;
+  double b;
+  double c;
+
+  public:
+  Trojkat(double a, double b, double c);
+
+  double Pole();
+  double Obwod();
+};
+
+
+class Kwadrat {
+private:
+  double bok;
+
+public:
+  Kwadrat(double bok);
+
+  double Pole() {}
+  double Obwod() {}
+};
+
+class Prostokat {
+private:
+  double szerokosc;
+  double dlugosc;
+
+public:
+  Prostokat(double szerokosc, double dlugosc);
+  double Pole();
+  double Obwod();
+
+};
+
+class Kolo {
+private:
+  double promien;
+public:
+  Kolo(double promien);
+  double Pole();
+  double Obwod();
+};
+
+#endif //ZADCLASSKCPP_FIGURA_H
