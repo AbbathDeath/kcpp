fa609aff60c04bbba8e11769f9ef3e33d73a2c9c
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 13:26:37 +0300

    przerobilem zadanie 2.5 do struktury klass

--- kcppZadania/ZadClassKcpp/include/ZadPrzeciazaniePotega.h
+++ kcppZadania/ZadClassKcpp/include/ZadPrzeciazaniePotega.h
@@ -1,8 +1,18 @@
-//
-// Created by death on 16.06.2026.
-//
 
 #ifndef ZADCLASSKCPP_ZADPRZECIAZANIEPOTEGA_H
 #define ZADCLASSKCPP_ZADPRZECIAZANIEPOTEGA_H
 
+
+#include "ZadKcpp.h"
+#include <string>
+
+class ZadPrzeciazaniePotega : public ZadKcpp {
+public:
+  void Uruchom() override;
+  std::string Opis() override;
+
+  int potega(int x, int y);
+
+  double potega(double x, int y);
+};
 #endif //ZADCLASSKCPP_ZADPRZECIAZANIEPOTEGA_H
--- kcppZadania/ZadClassKcpp/main.cc
+++ kcppZadania/ZadClassKcpp/main.cc
@@ -1,5 +1,10 @@
 #include "ZadMetodyAbstrakcyjne.h"
+#include "ZadKlasaKonstruktor.h"
+#include "ZadPrzekazywanieArgumentow.h"
+#include "ZadParzystosc.h"
+#include "ZadPrzeciazaniePotega.h"
 #include <iostream>
+
 
 
 
@@ -8,8 +13,13 @@ int main() {
 
 
   do {
-    std::cout << "Prosze wybrać zadanie z listy i podać numer zadania" << std::endl;
+    std::cout << "Proszę wybrać zadanie z listy i podać numer zadania" << std::endl;
     std::cout << "1. Metody abstrakcyjne" << std::endl;
+    std::cout << "2. Przyklady konstruktorów klasy" << std::endl;
+    std::cout << "3. Parzystość liczb" << std::endl;
+    std::cout << "4. Przekazywanie argumentów" << std::endl;
+    std::cout << "5. Potega liczb calkowitych i  zmiennoprzecinkowych" << std::endl;
+
     std::cout << "0. Wyjście" << std::endl;
     std::cin >> wybor;
 
@@ -24,10 +34,38 @@ int main() {
         break;
       }
 
+      case 2: {
+        ZadKlasaKonstruktor obj1;
+        std::cout << obj1.Opis() << std::endl;
+        obj1.Uruchom();
+        break;
+      }
+
+      case 3: {
+        ZadParzystosc obj1;
+        std::cout << obj1.Opis() << std::endl;
+        obj1.Uruchom();
+        break;
+      }
+
+      case 4: {
+        ZadPrzekazywanieArgumentow obj1;
+        std::cout << obj1.Opis() << std::endl;
+        obj1.Uruchom();
+        break;
+      }
+
+      case 5: {
+        ZadPrzeciazaniePotega obj1;
+        std::cout << obj1.Opis() << std::endl;
+        obj1.Uruchom();
+        break;
+      }
       default:
         std::cout << "Niepoprawny numer zadania" << std::endl;
         break;
     }
   }
   while (wybor != 0);
+  return 0;
 }
