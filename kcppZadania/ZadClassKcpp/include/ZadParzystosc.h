c0997e88730a3ca326ba6827f4476c0c6868b8e2
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 13:08:14 +0300

    przerobilem zadanie 4.7 do struktury klass

--- kcppZadania/ZadClassKcpp/include/ZadParzystosc.h
+++ kcppZadania/ZadClassKcpp/include/ZadParzystosc.h
@@ -1,8 +1,22 @@
-//
-// Created by death on 16.06.2026.
-//
 
 #ifndef ZADCLASSKCPP_ZADPARZYSTOSC_H
 #define ZADCLASSKCPP_ZADPARZYSTOSC_H
 
+#include "ZadKcpp.h"
+#include <string>
+class ZadParzystosc : public ZadKcpp {
+public:
+  void Uruchom() override;
+  std::string Opis() override;
+
+  void czyParzystaBitowe(int liczba);
+
+  void czyParzystaModulo(int liczba);
+
+  void czyParzystaWarunkowe(int liczba);
+
+  void CzyParzystaBitowo(int liczba);
+  void CzyParzystaModulo(int liczba);
+  void CzyParzystaWarunkowo(int liczba);
+};
 #endif //ZADCLASSKCPP_ZADPARZYSTOSC_H
---
+++ kcppZadania/ZadClassKcpp/src/ZadParzystosc.cc
@@ -0,0 +1,64 @@
+#include "ZadParzystosc.h"
+#include <iostream>
+#include <string>
+
+void ZadParzystosc::Uruchom() {
+  int liczba;
+  int wybor;
+  std::cout << "Proszę podać liczbe: " << std::endl;
+  std::cin >> liczba;
+  std::cout << "Proszę wybrać numer od 1 do 3 " << std::endl;
+  std::cout << "1. Sprawdzanie za pomocą operacji bitowych" << std::endl;
+  std::cout << "2. Sprawdzanie za pomocą modulo" << std::endl;
+  std::cout << "3. Sprawdzanie za pomocą operatora warunkowego" << std::endl;
+  std::cin >> wybor;
+
+  switch (wybor) {
+    case 1:
+      czyParzystaBitowe(liczba);
+      break;
+
+    case 2:
+      czyParzystaModulo(liczba);
+      break;
+
+    case 3:
+      czyParzystaWarunkowe(liczba);
+      break;
+
+    default:
+      std::cout << "Niepoprawny numer" << std::endl;
+  }
+}
+
+std::string ZadParzystosc::Opis() {
+  return "Program sprawdzający czy liczba jest parzysta/nieparzysta:";
+}
+
+void ZadParzystosc::czyParzystaBitowe(int liczba) {
+  int wynik = liczba & 1;
+  if (wynik == 0) {
+    std::cout << "Liczba jest parzysta" << std::endl;
+  }
+
+  else {
+    std::cout << "Liczba jest nieparzysta" << std::endl;
+  }
+
+}
+
+void ZadParzystosc::czyParzystaModulo(int liczba) {
+  if (liczba % 2 == 0) {
+    std::cout << "Liczba jest parzysta" << std::endl;
+  }
+
+
+  else {
+    std::cout << "Liczba jest nieparzysta" << std::endl;
+  }
+}
+
+void ZadParzystosc::czyParzystaWarunkowe(int liczba) {
+  liczba % 2 == 0 ? std::cout << "Liczba jest parzysta" : std::cout << "Liczba jest nieparzysta" << std::endl;
+}
+
