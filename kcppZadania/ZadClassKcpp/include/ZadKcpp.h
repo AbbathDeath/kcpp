7f6f7d7b32644d1fb5e171568e94136f91e3962a
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 09:38:42 +0300

    dodalem plik naglowkowy do klasy nadrzedna ZadKcpp

---
+++ kcppZadania/ZadClassKcpp/include/ZadKcpp.h
@@ -0,0 +1,17 @@
+
+
+#ifndef ZADKCPP_H
+#define ZADKCPP_H
+
+#include <string>
+class ZadKcpp {
+public:
+  virtual void Uruchom() = 0;
+  virtual std::string Opis() = 0;
+  virtual ~ZadKcpp();
+};
+
+#endif
+
+
+
