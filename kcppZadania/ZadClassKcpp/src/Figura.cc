13d9b44c634058fc47ba9a1eca7e3835d5dff116
AbbathDeath <death.abbath@yandex.ru>
2026-06-16 10:53:42 +0300

    dodalem dziedziczenie po klasie Figura

--- kcppZadania/ZadClassKcpp/src/Figura.cc
+++ kcppZadania/ZadClassKcpp/src/Figura.cc
@@ -1,4 +1,52 @@
-#include "ZadKcpp.h"
+#include "Figura.h"
+#include <cmath>
+Figura:: ~Figura () {}
 
-ZadKcpp::~ZadKcpp() {}
+Trojkat:: Trojkat (double a, double b, double c) {
+  this->a = a;
+  this->b = b;
+  this->c = c;
+}
+
+double Trojkat::Pole() {
+  double p = (a + b + c) / 2;
+  return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+double Trojkat::Obwod() {
+  return a + b + c;
+}
+
+Kwadrat::Kwadrat(double bok) {
+  this->bok = bok;
+}
+
+double Kwadrat::Pole() {
+  return bok * bok;
+
+}
+double Kwadrat::Obwod() {
+  return 4 * bok;
+}
+
+Kolo::Kolo(double promien) {
+  this->promien = promien;
+}
+
+double Kolo::Pole() {
+  return 3.14 * promien * promien;
+}
+
+double Kolo::Obwod() {
+  return 2 * 3.14 * promien;
+}
+
+double Kolo::Dystans(double x) {
+  double r = sqrt(x / 3.14);
+  return r;
+}
+
+
+
+
 
